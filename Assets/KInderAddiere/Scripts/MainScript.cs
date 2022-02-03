73afac5951b8cfc0762f9b65242b2d798ca26d69
NSaxx <90834343+NSaxx@users.noreply.github.com>
2022-02-03 10:29:37 +0100

    Add files via upload
    Assets und Projektsettings Update

--- Assets/KInderAddiere/Scripts/MainScript.cs
+++ Assets/KInderAddiere/Scripts/MainScript.cs
@@ -11,16 +11,14 @@ public class MainScript : MonoBehaviour
 
     [SerializeField] private InputField inputFieldval1, inputFieldval2;
     [SerializeField] private Text txtResult;
-
     // Start is called before the first frame update
     void Start()
     {
 
 
 
-        
     }
- 
+
 
     public int CheckAddition(int summandA, int summandB)
     {
@@ -29,8 +27,8 @@ public class MainScript : MonoBehaviour
         result = summandA + summandB;
         //Debug.Log(summandA + summandB);
         //Debug.Log(result);
+        txtResult.text = string.Format("{0}", result);
         return result;
-
     }
 
     public void GetValues()
@@ -38,9 +36,45 @@ public class MainScript : MonoBehaviour
         string val1, val2;
         val1 = inputFieldval1.text;
         val2 = inputFieldval2.text;
+        int summandA = -1, summandB = -1;
+        try
+        {
+            summandA = int.Parse(val1);
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("Bruh");
+            inputFieldval1.GetComponent<InputField>().image.color = Color.red;
+            inputFieldval1.GetComponent<InputField>().placeholder.GetComponent<Text>().text = "Bitte gültige Zahl eingeben.";
+            inputFieldval2.text = "";
+
+        }
+        try
+        {
+            summandB = int.Parse(val2);
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("Bruh");
+            inputFieldval2.GetComponent<InputField>().image.color = Color.red;
+            inputFieldval2.GetComponent<InputField>().placeholder.GetComponent<Text>().text = "Bitte gültige Zahl eingeben.";
+            inputFieldval2.text = "";
+
+        }
 
 
-        Debug.Log("Methode GetValues wird ausgeführt : " + CheckAddition(int.Parse(val1), int.Parse(val2)));
+        txtResult.text = CheckAddition(summandA, summandB).ToString();
+
+        if (summandA + summandB < summandA)
+        {
+            txtResult.text = "Kein Ergebnis.";
+            
+        }
+
+        if (summandA + summandB < summandB)
+        {
+            txtResult.text = "Kein Ergebnis.";
+        }
     }
 
     public void ResetButton()
@@ -48,11 +82,11 @@ public class MainScript : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+
     // Update is called once per frame
     void Update()
     {
- 
-    }
 
+    }
 
 }
